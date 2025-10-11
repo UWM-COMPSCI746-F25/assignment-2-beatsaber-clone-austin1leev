aeb2a4a98214a3370c8889896ea73568e1ceb479
austin1leev <Vandenp6@uwm.edu>
2025-10-10 20:03:00 -0500

    Readme and hands

--- assignment-2-beatsaber/scripts/Charactercontroller.gd
+++ assignment-2-beatsaber/scripts/Charactercontroller.gd
@@ -1,37 +1,16 @@
 extends Node3D
 
-@export var movement_velocity: float = 3.0
-@export var rotation_velocity: float = 90.0
-@export var jump_force: float = 5.0
-@export var gravity: float = -9.8
-@export var ground_height: float = 0.0  # Y position of the "ground"
+var xr_interface: XRInterface
 
-var vertical_velocity: float = 0.0
-var is_jumping: bool = false
+func _ready():
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		print("OpenXR initialized successfully")
 
-func _process(delta):
-	# --- Horizontal Movement ---
-	if Input.is_action_pressed("Forward"):
-		global_position += global_transform.basis.z * movement_velocity * delta
-	elif Input.is_action_pressed("Backward"):
-		global_position -= global_transform.basis.z * movement_velocity * delta
+		# Turn off v-sync!
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
-	if Input.is_action_pressed("Left"):
-		global_rotation_degrees.y += rotation_velocity * delta
-	elif Input.is_action_pressed("Right"):
-		global_rotation_degrees.y -= rotation_velocity * delta
-
-	# --- Jump logic ---
-	if Input.is_action_just_pressed("Jump") and not is_jumping:
-		is_jumping = true
-		vertical_velocity = jump_force
-
-	if is_jumping:
-		vertical_velocity += gravity * delta
-		global_position.y += vertical_velocity * delta
-
-		# Stop the jump when we hit the ground
-		if global_position.y <= ground_height:
-			global_position.y = ground_height
-			vertical_velocity = 0.0
-			is_jumping = false
+		# Change our main viewport to output to the HMD
+		get_viewport().use_xr = true
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
